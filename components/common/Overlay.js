96c5ec68f1b3847787304edc36bcf702ac159fea
Evgeniy Saigachenko <36506327+Yohanson555@users.noreply.github.com>
2022-06-23 15:38:33 +0300

    overlay implemented with react-modal

--- components/common/Overlay.js
+++ components/common/Overlay.js
@@ -4,20 +4,48 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
 
-const Overlay = ({ children, showClose, handleClose }) => {
+const customStyles = {
+    content: {
+        //top: '50%',
+        //left: '50%',
+        //right: 'auto',
+        //bottom: 'auto',
+        //marginRight: '-50%',
+        //transform: 'translate(-50%, -50%)',
+        padding: 0,
+        background: 'none',
+        border: 'none',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    overlay: {
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    }
+};
+
+const Overlay = ({ children, showClose, onClose }) => {
+    const handleClose = () => {
+        if (typeof onClose === 'function') {
+            onClose();
+        }
+    };
+
     return (
-        <div className="base-overlay">
-            {showClose ? <div className="base-overlay_close-button" onClick={handleClose}></div> : null}
+        <ReactModal isOpen={true} style={customStyles}>
+            {showClose ? <div className="overlay-close-button" onClick={handleClose}></div> : null}
             {children}
-        </div>
+        </ReactModal>
     );
 }
 
 Overlay.propTypes = {
     children: PropTypes.object.isRequired,
     showClose: PropTypes.bool,
-    handleClose: PropTypes.func,
+    onClose: PropTypes.func,
 };
 
 export default Overlay;
